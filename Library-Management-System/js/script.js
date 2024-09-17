1267a7210050b6f9cbb45bc266523c7c7586b320
Dhanapriya Gunasekaran <dhanapriya2022@gmail.com>
2024-09-17 09:23:45 +0530

    Chart Tried

--- Library-Management-System/js/script.js
+++ Library-Management-System/js/script.js
@@ -64,6 +64,7 @@ fetch("./../js/data.json")
 // DOM Elements
 const booksForm = document.getElementById("booksForm");
 const tableContainer = document.querySelector("#bookData");
+const dateInput = booksForm["date"];
 const titleInput = booksForm["title"];
 const authorInput = booksForm["author"];
 const yearInput = booksForm["year"];
@@ -71,25 +72,26 @@ const genreInput = booksForm["genre"];
 const ISBNInput = booksForm["ISBN"];
 const statusInput = booksForm["status"];
 
-const newBookList = JSON.parse(localStorage.getItem("newBookList")) || [];;
+const newBookList = JSON.parse(localStorage.getItem("newBookList")) || [];
 
-const addNewBook = (title, author, year, genre, ISBN, status) => {
+const addNewBook = (date, title, author, year, genre, ISBN, status) => {
     newBookList.push({
-        title, author, year, genre, ISBN, status
+        date, title, author, year, genre, ISBN, status
     });
 
     localStorage.setItem('newBookList', JSON.stringify(newBookList));
 
-    return{title, author, year, genre, ISBN, status};
+    return{date, title, author, year, genre, ISBN, status};
 };
 
 let listAdd = document.getElementById('listAdd');
 
 let row = document.createElement('tr');
 
-const createBookList = ({title, author, year, genre, ISBN, status}) => {
+const createBookList = ({date, title, author, year, genre, ISBN, status}) => {
 
     row.innerHTML = `
+        <td>${date}</td>
         <td>${title}</td>
         <td>${author}</td>
         <td>${year}</td>
@@ -115,18 +117,19 @@ booksForm.onsubmit = (e) => {
 
     if(editingBookList) {
         console.log(e);
-    }
+    } 
     const newReadyList = addNewBook(
+        dateInput.value,
         titleInput.value,
         authorInput.value,
         yearInput.value,
         genreInput.value,
         ISBNInput.value,
         statusInput.value
-    );
+    );    
 
     createBookList(newReadyList);
-
+    dateInput.value =   
     titleInput.value = "";
     authorInput.value = "";
     yearInput.value = "";
@@ -139,15 +142,41 @@ booksForm.onsubmit = (e) => {
 editRow = (e) => {
     console.log(e);
     editingBookList = e.parentElement.parentElement;
-    titleInput.value = editingBookList.cells[0].textContent;
-    authorInput.value = editingBookList.cells[1].textContent;
-    yearInput.value = editingBookList.cells[2].textContent;
-    genreInput.value = editingBookList.cells[3].textContent;
-    ISBNInput.value = editingBookList.cells[4].textContent;
-    statusInput.value = editingBookList.cells[5].textContent;
+    dateInput.value = editingBookList.cells[0].textContent;
+    titleInput.value = editingBookList.cells[1].textContent;
+    authorInput.value = editingBookList.cells[2].textContent;
+    yearInput.value = editingBookList.cells[3].textContent;
+    genreInput.value = editingBookList.cells[4].textContent;
+    ISBNInput.value = editingBookList.cells[5].textContent;
+    statusInput.value = editingBookList.cells[6].textContent;
 }
 deleteRow = (e) => {
     console.log("Delete clicked", e);
     listAdd.removeChild(row);
 }
 
+
+newBookList.forEach((listItems) => {
+    console.log(listItems);
+    const ctx = document.getElementById('myChartDisp').getContext('2d');
+    const myChartDisp = new Chart(ctx, {
+        type: 'bar', // Can be 'line', 'bar', 'pie', etc.
+        data: {
+            labels: listItems.title,
+            datasets: [{
+                label: 'Date',
+                data: listItems.title,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+})
